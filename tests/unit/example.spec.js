41ef3e4cee3e12083d563f45c2a2e8aa0705ec7d
Andrew Scott <andrew.scott@intellitect.com>
2018-12-17 15:37:29 -0800

    Repro steps

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,24 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+
+function configureVue(Vue) {
+  Vue.component('foo-true', {
+    render(h) { return h("span", "true!") }
+  })
+  Vue.component('foo-false', {
+    render(h) { return h("span", "false!") }
+  })
+
+}
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+    const vue = createLocalVue();
+    configureVue(vue);
+    const wrapper = mount(HelloWorld, {
+      localVue: vue
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.text()).toMatch("true")
   })
 })
